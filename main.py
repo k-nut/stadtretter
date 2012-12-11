cd2d042e9417cd37c4ca59ae0771d85f5c39c38e
Knut Hühne <knut@k-nut.eu>
2012-12-11 18:25:24 +0100

    Adapt for heroku

--- main.py
+++ main.py
@@ -71,5 +71,6 @@ def connect_db():
 
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.debug = False
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
