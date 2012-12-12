4c9eac87d24b8b04fa7e4f9700ba33c61b89b6e3
Knut Hühne <knut@k-nut.eu>
2012-12-12 09:15:56 +0100

    Enough of that debugging

--- main.py
+++ main.py
@@ -93,6 +93,6 @@ def findaddress(query):
         return jsonify()
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = False
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
